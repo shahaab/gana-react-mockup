b439e5ea39ac65b192cd88373eadb66dd42a7385
Shahaab Manzar <mshahaabmanzar@gmail.com>
2021-01-26 13:19:10 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 import Sidebar from './components/Sidebar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Overview from './pages/Overview';
-import { Reports, ReportsOne, ReportsTwo, ReportsThree } from './pages/Reports';
-import Team from './pages/Team';
 
 function App() {
   return (
@@ -11,11 +9,6 @@ function App() {
       <Sidebar />
       <Switch>
         <Route path='/overview' exact component={Overview} />
-        <Route path='/reports' exact component={Reports} />
-        <Route path='/reports/reports1' exact component={ReportsOne} />
-        <Route path='/reports/reports2' exact component={ReportsTwo} />
-        <Route path='/reports/reports3' exact component={ReportsThree} />
-        <Route path='/team' exact component={Team} />
       </Switch>
     </Router>
   );
